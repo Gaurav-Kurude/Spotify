9705058c5105f57f216b09e9c66af8489ae60ae0
Gaurav <gauravkurude.sitmech@gmail.com>
2025-09-30 18:58:30 +0530

    added

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`/${folder}/`);
+  let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -28,7 +28,7 @@ async function getSongs(folder) {
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href.split(`${folder}`)[1]);
+      songs.push(element.href.split(`/${folder}/`)[1]);
     }
   }
 
